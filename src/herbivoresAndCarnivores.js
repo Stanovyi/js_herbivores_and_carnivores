3f10e0ebe079fda467f14d270d412b3c9af9b29b
Ihor Stanovyi <ihor.stanovyi@gmail.com>
2025-08-03 19:09:13 +0300

    feat: Add Animal, Herbivore and Carnivore classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(food) {
+    Animal.alive.forEach((animal, idx) => {
+      if (
+        animal.name === food.name &&
+        animal.health > 0 &&
+        animal.hidden === false
+      ) {
+        animal.health -= 50;
+      }
+    });
+
+    Animal.alive = Animal.alive.filter((animal) => {
+      if (animal.health >= 0 && animal !== food) {
+        return animal;
+      }
+    });
+  }
 }
 
 module.exports = {
